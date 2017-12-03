fd78b38899c86b7b746353a300a60bba31e58cfd
David Zhao <davidfanzhao@gmail.com>
2017-12-03 18:20:30 -0500

    set up prediction for specific country

--- predict_missing.py
+++ predict_missing.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import LinearRegression, LogisticRegression, HuberRegressor, Ridge
-from sklearn.svm import LinearSVC, LinearSVR
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import HuberRegressor
+from sklearn.neighbors import KNeighborsRegressor
 
 
 def main():
@@ -9,29 +11,26 @@ def main():
   data = pd.read_csv("clean_missing_data.csv", sep=",")
   missing_data = pd.read_csv("missing_data.csv", sep=",")
 
-  y_gender = data["MALE"]
-  y_age = data["age"]
+  y = data["age"]
   X = data.drop(["MALE", "FEMALE", "age"], axis=1)
   X_missing = missing_data.drop(["MALE", "FEMALE", "age"], axis=1)
 
-  print("[INFO] training gender model...")
-  model = LogisticRegression(C=5)
-  scores = cross_val_score(model, X, y_gender, cv=10)
-  print(scores)
-
-  print("[INFO] predicting genders...")
-  model.fit(X, y_gender)
-  preds_gender = model.predict(X_missing)
-
   print("[INFO] training age model...")
-  # model = LinearRegression(n_jobs=8)
-  model = Ridge(alpha=10)
-  scores = cross_val_score(model, X, y_age, cv=10)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+  # model = HuberRegressor()
+  model = RandomForestRegressor(n_estimators=10, max_features="sqrt", n_jobs=8)
+  scores = cross_val_score(model, X_train, y_train, cv=10)
   print(scores)
+  model.fit(X_train, y_train)
+  preds = model.predict(X_train)
+  print("train error:", np.sum((preds - y_train)**2) / len(y_train))
+  preds = model.predict(X_test)
+  print("test error:", np.sum((preds - y_test)**2) / len(y_test))
 
   print("[INFO] predicting ages...")
-  model.fit(X, y_age)
-  preds_age = model.predict(X_missing)
+  model.fit(X, y)
+  preds = model.predict(X_missing)
+  np.savetxt("glrm_results.csv", preds, delimiter=',')
 
 
 if __name__ == "__main__":
